b8d5324997bc1c98f27671ce5bf72b7004ae9cec
ouliyuan <ouliyuan@didichuxing.com>
2017-11-16 10:29:47 +0800

    增加pageTotal

--- springframework-ext-common/src/main/java/org/springframework/ext/module/response/PageResponse.java
+++ springframework-ext-common/src/main/java/org/springframework/ext/module/response/PageResponse.java
@@ -10,4 +10,28 @@ public class PageResponse<T> extends Response<List<T>> {
     private int size;
     private int pageNum;
     private int pageSize;
+
+    public void setPageNum(int pageNum) {
+        if (pageNum > 0) {
+            this.pageNum = pageNum;
+        }
+    }
+
+    public void setPageSize(int pageSize) {
+        if (pageSize > 0) {
+            this.pageSize = pageSize;
+        }
+    }
+
+    public Integer getPageTotal() {
+        int total = getTotal();
+        int pageSize = getPageSize();
+
+        int pageTotal = total / pageSize;
+
+        if (total % pageSize > 0) {
+            pageTotal += 1;
+        }
+        return pageTotal;
+    }
 }
