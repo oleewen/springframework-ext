9a350e69951bd1504a2deba27be633021051a46a
ouliyuan <ouliyuan@didichuxing.com>
2017-11-22 11:15:22 +0800

    gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);

--- springframework-ext-common/src/main/java/org/springframework/ext/common/helper/JsonHelper.java
+++ springframework-ext-common/src/main/java/org/springframework/ext/common/helper/JsonHelper.java
@@ -2,6 +2,7 @@ package org.springframework.ext.common.helper;
 
 import com.google.common.collect.Lists;
 import com.google.common.reflect.TypeToken;
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.TypeAdapter;
@@ -25,7 +26,7 @@ import java.util.Map;
  */
 public abstract class JsonHelper {
     /** json处理器 */
-    private static Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+    private static Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).disableHtmlEscaping().create();
 
     /**
      * 将object对象转为json字符串
